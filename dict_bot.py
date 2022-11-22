029130138fd67369d2588c23f2dbc69f47475e56
Alexey <113031830+AlexBorQA@users.noreply.github.com>
2022-11-22 23:07:59 +0500

    Update dict_bot.py

--- dict_bot.py
+++ dict_bot.py
@@ -5,7 +5,7 @@
 
 from telebot import TeleBot, types
 
-bot = TeleBot(token='5980038034:AAFmMlT_srIdYZAdeUygJpK6mZCUSPnev8o', parse_mode='html') # создание бота
+bot = TeleBot(token='5971673107:AAETMPOOcOdqmTW_DePUds44lNz_1Xhi-dc', parse_mode='html') # создание бота
 
 # словарь с определениями и аббревиатурами, которые знает бот
 # в формате:
