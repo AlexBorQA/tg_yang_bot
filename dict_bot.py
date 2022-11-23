abdc6c59d239b1cf723489cb1319fbe15a3a942c
Alexey <113031830+AlexBorQA@users.noreply.github.com>
2022-11-23 13:20:47 +0500

    Update dict_bot.py
    Убрал токен

--- dict_bot.py
+++ dict_bot.py
@@ -5,7 +5,7 @@
 
 from telebot import TeleBot, types
 
-bot = TeleBot(token='5971673107:AAETMPOOcOdqmTW_DePUds44lNz_1Xhi-dc', parse_mode='html') # создание бота
+bot = TeleBot(token='', parse_mode='html') # создание бота
 
 # словарь с определениями и аббревиатурами, которые знает бот
 # в формате:
